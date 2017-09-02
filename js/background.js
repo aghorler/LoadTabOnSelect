1f5ae2d6e4c72127bd9ca02969fe08f2b411edec
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-02 20:46:32 +1000

    Remove logging to console for release to AMO.

--- js/background.js
+++ js/background.js
@@ -9,10 +9,8 @@ function handleUpdated(tabId, changeInfo, tabInfo){
 	var i;
 	for(i = 0; i < managedTabs.length; i++){
 		if(managedTabs[i].id == tabId && changeInfo.url !== undefined && changeInfo.url !== "about:blank" && managedTabs[i].loadedUrl == null){
-			console.log("initialUrl: " + managedTabs[i].initialUrl + ". For " + managedTabs[i].id);
 
 			managedTabs[i].loadedUrl = changeInfo.url;
-			console.log("loadedUrl: " + managedTabs[i].loadedUrl);
 
 			browser.tabs.update(managedTabs[i].id, {
 				url: "about:blank"
@@ -28,16 +26,12 @@ function handleActivated(activeInfo){
 	for(i = 0; i < managedTabs.length; i++){
 		if(managedTabs[i].id == activeInfo.tabId){
 			console.log(JSON.stringify(managedTabs));
-			
+
 			browser.tabs.update(managedTabs[i].id, {
 				url: managedTabs[i].loadedUrl
-			})
-
-			console.log("Removing tab: " + managedTabs[i].id);
+			});
 
 			managedTabs.splice(i, 1);
-
-			console.log(JSON.stringify(managedTabs));
 
 			break;
 		}
